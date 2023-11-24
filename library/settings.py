e9a43f3e990f902f0fb77d2766c9a8f43b9c3186
ulugbekusmanovdev <uusmanov928@gmail.com>
2023-11-24 11:24:50 +0600

    done

--- library/settings.py
+++ library/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'embed_video', 
-    # 'modeltranslation',
+    'modeltranslation',
 
 
     'home.apps.HomeConfig',
